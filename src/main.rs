cf845e45acb77697027feb095283ae0b7213eb60
Max Holder <mxhold@gmail.com>
2016-03-19 11:37:26 -0700

    Extract print_state

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,31 @@ fn get<T>(v: &Vec<Vec<T>>, y: i32, x: i32) -> Option<&T> {
     }
 }
 
+fn alive_neighbor_count(v: &Vec<Vec<bool>>, y: usize, x: usize) -> usize {
+    let x = x as i32;
+    let y = y as i32;
+    let neighbors = vec![
+        get(&v, y - 1, x - 1), get(&v, y - 1, x), get(&v, y - 1, x + 1),
+        get(&v, y,     x - 1),                    get(&v, y,     x + 1),
+        get(&v, y + 1, x - 1), get(&v, y + 1, x), get(&v, y + 1, x + 1),
+    ];
+
+    neighbors.iter().filter(|x| x.is_some()).filter(|x| *x.unwrap()).collect::<Vec<_>>().len()
+}
+
+fn print_state(v: &Vec<Vec<bool>>) {
+    for line in v {
+        for &cell in line {
+            if cell {
+                print!("X");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+}
+
 fn main() {
     let initial_state = vec![
         vec![false, true, false],
@@ -20,36 +45,16 @@ fn main() {
 
     for (y, line) in initial_state.iter().enumerate() {
         for (x, cell) in line.iter().enumerate() {
-            let x = x as i32;
-            let y = y as i32;
-            let neighbors = vec![
-                get(&initial_state, y - 1, x - 1), get(&initial_state, y - 1, x), get(&initial_state, y - 1, x + 1),
-                get(&initial_state, y,     x - 1),                                get(&initial_state, y,     x + 1),
-                get(&initial_state, y + 1, x - 1), get(&initial_state, y + 1, x), get(&initial_state, y + 1, x + 1),
-            ];
 
-            let alive_neighbor_count = neighbors.iter().filter(|x| x.is_some()).filter(|x| *x.unwrap()).collect::<Vec<_>>().len();
-
-            let new_cell = match (*cell, alive_neighbor_count) {
+            let new_cell = match (*cell, alive_neighbor_count(&initial_state, y, x)) {
                 (true, 2) => true,
                 (_, 3) => true,
                 (_, _) => false,
             };
 
             new_state[y as usize][x as usize] = new_cell;
-
-            //println!("({}, {}): {} with {:?} = {}", x, y, *cell, alive_neighbor_count, new_cell);
         }
     }
 
-    for line in new_state {
-        for cell in line {
-            if cell {
-                print!("X");
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
+    print_state(&new_state);
 }
